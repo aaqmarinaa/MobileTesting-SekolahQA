f95225bdf6c07e67ecad7025511f3831f42839be
aaqmarinaa <almasaqmarina@gmail.com>
2020-11-25 22:26:19 +0700

    mobile testing

---
+++ Test Suites/Spacestock/Login - Add Contact.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login - Add Contact</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>033770ff-676a-4bc9-82c4-8bd1fd3c733e</testSuiteGuid>
+   <testCaseLink>
+      <guid>b80f33df-87fa-4c39-99a3-8a1c7fb489e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Spacestock/Login Spacestock</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>847d03af-11a0-4831-b263-f3c53d90af8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Spacestock/Add Contact - Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Spacestock/Login - Search.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Login untul search user</description>
+   <name>Login - Search</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b268f023-56c4-4430-aac3-9c8bd568f5b0</testSuiteGuid>
+   <testCaseLink>
+      <guid>eaf21fe8-5bf7-4e01-859b-2a31c366d834</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Spacestock/Login Spacestock</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f9fffd6-b917-422d-83df-66c9c50ad1bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Spacestock/Search</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
